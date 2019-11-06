38dc0f06074ab2ea8f418095c8bda6d1317d21e2
Bill <bill.chandos@gmail.com>
2019-11-05 19:52:14 -0800

    prevent default on submit

--- src/TaskForm.js
+++ src/TaskForm.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './TaskForm.css';
 
+function submitNewItem(e) {
+    e.preventDefault();
+}
+
 function TaskForm() {
     return (
         <div>
             <h3>Add new task</h3>
-            <form>
+            <form onSubmit={submitNewItem}>
                 <input className="TaskForm-text-input" type="text" name="newTaskItem"/>
+                <button className="TaskForm-button" type="submit" name="newTaskButton">Add</button>
             </form>
         </div>
     )
